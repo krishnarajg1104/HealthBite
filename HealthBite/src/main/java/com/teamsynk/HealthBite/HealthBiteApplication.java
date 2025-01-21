4eec7ebfb2762509652c89b72b2aa7788d72a329
Krishnaraj G <132008512+krishnarajg1104@users.noreply.github.com>
2025-01-21 15:50:16 +0530

    Add files via upload
    Initialized Project

---
+++ HealthBite/src/main/java/com/teamsynk/HealthBite/HealthBiteApplication.java
@@ -0,0 +1,13 @@
+package com.teamsynk.HealthBite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthBiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthBiteApplication.class, args);
+	}
+
+}
---
+++ HealthBite/src/test/java/com/teamsynk/HealthBite/HealthBiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teamsynk.HealthBite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthBiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
